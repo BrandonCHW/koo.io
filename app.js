57db547e847ddcc960dd1541cbc5dce29fa3843c
BrandonCHW <chanhingwah.brandon@gmail.com>
2020-07-08 17:38:30 -0400

    setup socketio following doc

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require('express');
-const app = express();
-const PORT = 2000;
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+const PORT = 3000
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/main.html");
+    res.sendFile(__dirname + "/views/main.html")
 });
 
 app.get("*", (req,res) => {
-    res.send("Is this the real life? Is this just fantasy? (ERROR PAGE)");
+    res.send("Is this the real life? Is this just fantasy? (ERROR PAGE)")
 });
 
-app.listen(PORT, () => {
-    console.log(`Koo.io server started on port ${PORT}`);
+io.on('connection', (socket) => {
+    console.log("new socket connection")
+    socket.on('disconnect', () => {
+        console.log("socket disconnected")
+    })
+});
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 });
