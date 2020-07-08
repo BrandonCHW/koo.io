854af5582832812a96e67571f6f064a2aa65ffed
BrandonCHW <chanhingwah.brandon@gmail.com>
2020-07-08 16:39:42 -0400

    placeholder main page

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 2000;
 
+app.get("/", (req,res) => {
+    res.send("Koo io main page");
+});
+
+app.get("*", (req,res) => {
+    res.send("Is this the real life? Is this just fantasy? (ERROR PAGE)");
+});
+
 app.listen(PORT, () => {
     console.log(`Koo.io server started on port ${PORT}`);
 });
