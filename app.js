f0d4bb64bde38aef39693966c8d0edc03d20be06
BrandonCHW <brad_1800@hotmail.com>
2020-07-13 01:06:03 -0400

    create simple payload exchange client->server

--- app.js
+++ app.js
@@ -9,17 +9,27 @@ app.get("/", (req,res) => {
     res.sendFile(__dirname + "/client/main.html")
 });
 
-app.get("*", (req,res) => {
-    res.send("Error")
-});
+const SOCKET_LIST = {};
 
 io.on('connection', (socket) => {
-    console.log("new socket connection")
-    socket.on('disconnect', () => {
-        console.log("socket disconnected")
+    socket.on('action payload', (obj) => {
+        console.log(obj)
     })
+    // console.log("new socket connection")
+    socket.on('disconnect', () => {
+        // console.log("socket disconnected")
+    })
+});
+
+app.get("*", (req,res) => {
+    res.send("Error")
 });
 
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 });
+
+setInterval(function() {
+    // console.log("hello")
+    
+},1000)
